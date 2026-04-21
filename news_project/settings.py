82abc51e28157ab5d98c0ddc2fd1f143dcfee749
NickDenji <ndionissiou@gmail.com>
2026-04-21 19:20:07 +0100

    Final working version with Docker and documentation

--- news_project/settings.py
+++ news_project/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = "news_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "news_db",
-        "USER": "news_user",
-        "PASSWORD": "password123",
-        "HOST": "localhost",
-        "PORT": "3306",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
